dacb42c7a32cf614321ee4fe615061d66621be11
checkshumm <checkshumm@gmail.com>
2018-04-03 22:09:42 -0400

    Expiriation date to database, modified UI

--- app/src/main/java/com/example/nathanshumm/gympass/PublicPayment.java
+++ app/src/main/java/com/example/nathanshumm/gympass/PublicPayment.java
@@ -1,19 +1,25 @@
 package com.example.nathanshumm.gympass;
 
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class PublicPayment extends AppCompatActivity {
 
+    private Window window;
     Button doneButton;
     Button regisClassButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_public_payment);
+
+        window = this.getWindow();
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorBurgundy));
 
         doneButton=(Button)findViewById(R.id.btn_done);
         doneButton.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/nathanshumm/gympass/yogaActivity.java
+++ app/src/main/java/com/example/nathanshumm/gympass/yogaActivity.java
@@ -1,10 +1,12 @@
 package com.example.nathanshumm.gympass;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -16,6 +18,7 @@ public class yogaActivity extends AppCompatActivity implements View.OnClickListe
 
     private Toolbar toolbar;
     private Button yogaButton;
+    private Window window;
 
     // Database instance
     private FirebaseDatabase database;
@@ -27,6 +30,9 @@ public class yogaActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yoga);
+
+        window = this.getWindow();
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorBurgundy));
 
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
